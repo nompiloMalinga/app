85a366cf3033b245d5bf2602b29be5c67a7d3278
nompiloMalinga <nompilorwmalinga@gmail.com>
2026-03-02 17:26:48 +0200

    adding error handling

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Converter } from './service/converter';
+import { retry } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -15,13 +16,27 @@ export class App {
   email = '';
   url = '';
   response:any;
+  errorMessage: string | null = null;
+  isLoading = false;
 
   constructor(private con: Converter){}
 
   submit(){
-    this.con.testEndpoint(this.url,this.email)
-    .subscribe(res =>{
-      this.response = res
-    })
-  }
+    this.isLoading = true;
+    this.errorMessage = null;
+    this.response = null;
+
+    this.con.testEndpoint(this.url, this.email)
+      .pipe(retry(2))
+      .subscribe({
+        next: (res) => {
+          this.response = res;
+          this.isLoading = false;
+        },
+        error: (err) => {
+          this.errorMessage = err?.message || "Connection failed.";
+          this.isLoading = false;
+        }
+      });
+}
 }
